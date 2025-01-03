1076c72876103d8727e47878a1b65bdff756cbcb
Erivan Couttolenc 2024 <rentalcarapplimitlessslimitles@gmail.com>
2025-01-03 10:05:49 +0100

    new
    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import {redirect} from 'next/navigation';
+import localFont from '@next/font/local';
+
+const Es = localFont({
+  src: '../fonts/es.ttf',
+});
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
   redirect('/fr');

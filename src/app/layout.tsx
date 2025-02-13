c9940ed3d8465a8a3df7f3e5640c46ba5f128ad4
Erivan Couttolenc 2024 <rentalcarapplimitlessslimitles@gmail.com>
2025-02-13 22:42:55 +0100

    nex
    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
-import { ReactNode } from 'react';
-import localFont from 'next/font/local'; // Correction de l'import
-
-// Déclaration de la police (remplace le chemin par le bon)
-
-type Props = {
-  children: ReactNode;
-};
-
-export default function RootLayout({ children }: Props) {
-  return (
-    <html lang="en" dir='ltr'>
-      {children}
-    </html>
-  );
+export default function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  return <>{children}</>;
 }

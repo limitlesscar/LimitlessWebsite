0f701b1689195763d0b7665fc8a10369869f15db
limitlesscar <rentalcarapplimitlessslimitles@gmail.com>
2025-02-14 07:43:38 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-
 import { ReactNode } from 'react';
 
 type Props = {
@@ -9,7 +7,6 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
-=======
 import { ReactNode, ReactElement } from 'react';
 
 type Props = {
@@ -19,6 +16,5 @@ type Props = {
 // A layout is required for Next.js projects with a `not-found.tsx` page at the root.
 // This layout simply passes the `children` through to the page.
 export default function RootLayout({ children }: Props): ReactElement {
->>>>>>> Stashed changes
   return <>{children}</>;
 }

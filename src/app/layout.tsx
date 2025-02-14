4575f52e37c1534dccb77efb4115ff6edf8fcab1
limitlesscar <rentalcarapplimitlessslimitles@gmail.com>
2025-02-14 08:01:47 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 import { ReactNode } from 'react';
 
 type Props = {
-  children: ReactNode;
+  children: ReactNode; // `ReactNode` allows anything that can be rendered in React
 };
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
-import { ReactNode, ReactElement } from 'react';
-
-type Props = {
-  children: ReactNode; // `ReactNode` allows anything that can be rendered in React, but can be more specific
-};
-
-// A layout is required for Next.js projects with a `not-found.tsx` page at the root.
-// This layout simply passes the `children` through to the page.
-export default function RootLayout({ children }: Props): ReactElement {
-  return <>{children}</>;
+  return children;
 }
